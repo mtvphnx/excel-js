dd1278c29b97bfec1f9300efba5ffcf53c5093d8
mtvphnx <matvey.phoen1x@yandex.ru>
2020-05-15 01:58:23 +0500

    scss + babel

--- webpack.config.js
+++ webpack.config.js
@@ -25,16 +25,36 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'index.html'
         }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, './src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')
-                }
-            ],
-        }),
+        new CopyPlugin([
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }
+        ]),
         new MiniCssExtractPlugin({
             filename: 'bundle.[hash].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    }
 };
